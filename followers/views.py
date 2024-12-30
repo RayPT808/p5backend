e0ed1e27516f4ed140980680deb916bde0fddba7
RayPT808 <qiauxirt@students.codeinstitute.net>
2024-12-30 16:27:43 +0000

    Added extra fileds to Posts and Profiles.

--- followers/views.py
+++ followers/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, permissions
-from drf_api.permissions import IsOwnerOrReadOnly
+from p5backend.permissions import IsOwnerOrReadOnly
 from .models import Follower
 from .serializers import FollowerSerializer
 
