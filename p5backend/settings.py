e8323994aea938cb306e0cc389f811e0b27a0cfa
RayPT808 <qiauxirt@students.codeinstitute.net>
2025-01-05 12:18:59 +0000

    Preparing for deployment vol. 4.

--- p5backend/settings.py
+++ p5backend/settings.py
@@ -60,9 +60,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-raypt808-p5backend-si1bgp1jdzg.ws-eu117.gitpod.io',
-                 '8000-raypt808-p5backend-lqnmw5t1rmu.ws-eu117.gitpod.io',
-                 'localhost', 'p5backend.herokuapp.com'
+ALLOWED_HOSTS = [
+'8000-raypt808-p5backend-si1bgp1jdzg.ws-eu117.gitpod.io',
+'8000-raypt808-p5backend-lqnmw5t1rmu.ws-eu117.gitpod.io',
+'localhost',
+'p5backend-9681141c47d0.herokuapp.com'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
